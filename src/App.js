f3b80dfb89bc5a7f2a561c499880256f362b406e
Toyin Dawudu <toyindawudu@gmail.com>
2022-02-20 23:16:40 +0000

    Working on building the contact list component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
-{/* import ContactList from "./components/ContactList"; */}
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
-    <div>
+    <div className='ui container'>
       <Header />
       <AddContact />
-       {/* <ContactList /> */} 
+      <ContactList /> 
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,8 +3,8 @@ import React from "react";
 const ContactList = () => {
 
   return(
-    <div className="ui container">
-      <Header />
+    <div className="ui celled list">
+      <p>Contact List</p>
     </div>
   );
 }
